6252250681c105afc4749137fc037dc9522b59f3
Carlos Tocarruncho <112905487+carlospalacios25@users.noreply.github.com>
2024-09-23 21:46:57 -0500

    Update taller.sql

--- taller.sql
+++ taller.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 13-08-2024 a las 04:42:34
+-- Tiempo de generación: 24-09-2024 a las 04:46:01
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -20,237 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `taller`
 --
-
-DELIMITER $$
---
--- Procedimientos
---
-CREATE DEFINER=`root`@`localhost` PROCEDURE `ActualizarCliente` (IN `p_cedula` INT(20), IN `p_nom_cliente` VARCHAR(50), IN `p_ape_cliente` VARCHAR(50), IN `p_telefono` BIGINT(15), IN `p_direccion` VARCHAR(70))   BEGIN
-    UPDATE cliente
-    SET nom_cliente = p_nom_cliente,
-        ape_cliente = p_ape_cliente,
-        telefono = p_telefono,
-        direccion = p_direccion
-    WHERE cedula = p_cedula;
-END$$
-
-CREATE DEFINER=`root`@`localhost` PROCEDURE `ActualizarCompra` (IN `p_id_compra` VARCHAR(20), IN `p_doc_proveedor` BIGINT, IN `p_id_producto` INT, IN `p_precio_total` DOUBLE, IN `p_fecha_compra` DATE, IN `p_cantidad` INT)   BEGIN
-    UPDATE compra 
-    SET doc_proveedor = p_doc_proveedor, 
-        id_producto = p_id_producto, 
-        precio_total = p_precio_total, 
-        fecha_compra = p_fecha_compra, 
-        cantidad = p_cantidad
-    WHERE id_compra = p_id_compra;
-END$$
-
-CREATE DEFINER=`root`@`localhost` PROCEDURE `ActualizarEmpleado` (IN `p_documento_emp` INT, IN `p_nom_empleado` VARCHAR(50), IN `p_ape_empleado` VARCHAR(50), IN `p_direccion` VARCHAR(50), IN `p_telefono` BIGINT, IN `p_id_cargos` INT)   BEGIN
-    UPDATE empleado 
-    SET nom_empleado = p_nom_empleado, 
-        ape_empleado = p_ape_empleado, 
-        direccion = p_direccion, 
-        telefono = p_telefono, 
-        id_cargos = p_id_cargos
-    WHERE documento_emp = p_documento_emp;
-END$$
-
-CREATE DEFINER=`root`@`localhost` PROCEDURE `ActualizarFactura` (IN `p_id_factura` INT, IN `p_id_servicio` INT, IN `p_id_producto` INT, IN `p_precio_total` FLOAT, IN `p_descuento` INT, IN `p_fecha_factura` DATE, IN `p_cantidad` INT)   BEGIN
-    UPDATE factura
-    SET id_servicio = p_id_servicio,
-        id_producto = p_id_producto,
-        precio_total = p_precio_total,
-        descuento = p_descuento,
-        fecha_factura = p_fecha_factura,
-        cantidad = p_cantidad
-    WHERE id_factura = p_id_factura;
-END$$
-
-CREATE DEFINER=`root`@`localhost` PROCEDURE `ActualizarProducto` (IN `p_id_producto` INT, IN `p_nom_producto` VARCHAR(45), IN `p_codigo` VARCHAR(45), IN `p_descripcion` VARCHAR(45), IN `p_precio_unitario` INT, IN `p_existencias` INT)   BEGIN
-    UPDATE producto 
-    SET nom_producto = p_nom_producto,
-        codigo = p_codigo,
-        descripcion = p_descripcion,
-        precio_unitario = p_precio_unitario,
-        existencias = p_existencias
-    WHERE id_producto = p_id_producto;
-END$$
-
-CREATE DEFINER=`root`@`localhost` PROCEDURE `ActualizarProveedor` (IN `p_documento_NIT` BIGINT(16), IN `p_nom_proveedor` VARCHAR(70), IN `p_apellido_sociedad` VARCHAR(70), IN `p_direccion` VARCHAR(100), IN `p_telefono` BIGINT(15), IN `p_id_tipo_proveedor` INT(10))   BEGIN
-    UPDATE proveedor 
-    SET nom_proveedor = p_nom_proveedor,
-        apellido_sociedad = p_apellido_sociedad,
-        direccion = p_direccion,
-        telefono = p_telefono,
-        id_tipo_proveedor = p_id_tipo_proveedor
-    WHERE documento_NIT = p_documento_NIT;
-END$$
-
-CREATE DEFINER=`root`@`localhost` PROCEDURE `ActualizarServicio` (IN `p_id_servicios` INT, IN `p_observaciones` VARCHAR(200), IN `p_mantenimiento` VARCHAR(70), IN `p_cedula_cliente` INT, IN `p_documento_emp` INT)   BEGIN
-    UPDATE servicios 
-    SET observaciones = p_observaciones, 
-        mantenimiento = p_mantenimiento, 
-        cedula_cliente = p_cedula_cliente, 
-        documento_emp = p_documento_emp
-    WHERE id_servicios = p_id_servicios;
-END$$
-
-CREATE DEFINER=`root`@`localhost` PROCEDURE `ActualizarTipoProveedor` (IN `p_id_tipo_proveedor` INT, IN `p_tipo_proveedor` VARCHAR(50), IN `p_descripcion` VARCHAR(70))   BEGIN
-    UPDATE tipo_proveedor 
-    SET tipo_proveedor = p_tipo_proveedor, descripcion = p_descripcion 
-    WHERE id_tipo_proveedor = p_id_tipo_proveedor;
-END$$
-
-CREATE DEFINER=`root`@`localhost` PROCEDURE `Actualizar_Cargo` (IN `carg_id` INT, IN `carg_nombre` VARCHAR(50))   BEGIN
-    UPDATE cargos SET tipo_cargo = carg_nombre WHERE id_cargos = carg_id;
-END$$
-
-CREATE DEFINER=`root`@`localhost` PROCEDURE `ConsultarClientes` ()   BEGIN
-    SELECT * FROM cliente;
-END$$
-
-CREATE DEFINER=`root`@`localhost` PROCEDURE `ConsultarCompra` ()   BEGIN
-    SELECT * FROM compra;
-END$$
-
-CREATE DEFINER=`root`@`localhost` PROCEDURE `ConsultarEmpleado` (IN `p_documento_emp` INT)   BEGIN
-    SELECT * FROM empleado WHERE documento_emp = p_documento_emp;
-END$$
-
-CREATE DEFINER=`root`@`localhost` PROCEDURE `ConsultarFactura` (IN `p_id_factura` INT)   BEGIN
-    SELECT * FROM factura WHERE id_factura = p_id_factura;
-END$$
-
-CREATE DEFINER=`root`@`localhost` PROCEDURE `ConsultarProductos` ()   BEGIN
-    SELECT * FROM producto;
-END$$
-
-CREATE DEFINER=`root`@`localhost` PROCEDURE `ConsultarProveedor` (IN `p_documento_NIT` BIGINT(16))   BEGIN
-    SELECT * FROM proveedor WHERE documento_NIT = p_documento_NIT;
-END$$
-
-CREATE DEFINER=`root`@`localhost` PROCEDURE `ConsultarServicio` (IN `p_id_servicios` INT)   BEGIN
-    SELECT * FROM servicios WHERE id_servicios = p_id_servicios;
-END$$
-
-CREATE DEFINER=`root`@`localhost` PROCEDURE `ConsultarTipoProveedor` ()   BEGIN
-    SELECT * FROM tipo_proveedor;
-END$$
-
-CREATE DEFINER=`root`@`localhost` PROCEDURE `Consulta_Cargo` (IN `con_Cargo` VARCHAR(50))   BEGIN
-    SELECT * FROM cargos WHERE tipo_cargo = con_Cargo;
-END$$
-
-CREATE DEFINER=`root`@`localhost` PROCEDURE `EliminarCliente` (IN `p_cedula` INT(20))   BEGIN
-    DELETE FROM cliente WHERE cedula = p_cedula;
-END$$
-
-CREATE DEFINER=`root`@`localhost` PROCEDURE `EliminarCompra` (IN `p_id_compra` VARCHAR(20))   BEGIN
-    DELETE FROM compra WHERE id_compra = p_id_compra;
-END$$
-
-CREATE DEFINER=`root`@`localhost` PROCEDURE `EliminarEmpleado` (IN `p_documento_emp` INT)   BEGIN
-    DELETE FROM empleado WHERE documento_emp = p_documento_emp;
-END$$
-
-CREATE DEFINER=`root`@`localhost` PROCEDURE `EliminarFactura` (IN `p_id_factura` INT)   BEGIN
-    DELETE FROM factura WHERE id_factura = p_id_factura;
-END$$
-
-CREATE DEFINER=`root`@`localhost` PROCEDURE `EliminarProducto` (IN `p_id_producto` INT)   BEGIN
-    DELETE FROM producto WHERE id_producto = p_id_producto;
-END$$
-
-CREATE DEFINER=`root`@`localhost` PROCEDURE `EliminarProveedor` (IN `p_documento_NIT` BIGINT(16))   BEGIN
-    DELETE FROM proveedor WHERE documento_NIT = p_documento_NIT;
-END$$
-
-CREATE DEFINER=`root`@`localhost` PROCEDURE `EliminarServicio` (IN `p_id_servicios` INT)   BEGIN
-    DELETE FROM servicios WHERE id_servicios = p_id_servicios;
-END$$
-
-CREATE DEFINER=`root`@`localhost` PROCEDURE `EliminarTipoProveedor` (IN `p_id_tipo_proveedor` INT)   BEGIN
-    DELETE FROM tipo_proveedor WHERE id_tipo_proveedor = p_id_tipo_proveedor;
-END$$
-
-CREATE DEFINER=`root`@`localhost` PROCEDURE `Eliminar_Cargo` (IN `carg_id` INT)   BEGIN
-    DELETE FROM cargos WHERE id_cargos = carg_id;
-END$$
-
-CREATE DEFINER=`root`@`localhost` PROCEDURE `Insertar` (IN `In_tipo_cargo` VARCHAR(50))   BEGIN
-    INSERT INTO cargos (tipo_cargo) VALUES (In_tipo_cargo);
-END$$
-
-CREATE DEFINER=`root`@`localhost` PROCEDURE `InsertarCliente` (IN `p_cedula` INT(20), IN `p_nom_cliente` VARCHAR(50), IN `p_ape_cliente` VARCHAR(50), IN `p_telefono` BIGINT(15), IN `p_direccion` VARCHAR(70))   BEGIN
-    INSERT INTO cliente (cedula, nom_cliente, ape_cliente, telefono, direccion)
-    VALUES (p_cedula, p_nom_cliente, p_ape_cliente, p_telefono, p_direccion);
-END$$
-
-CREATE DEFINER=`root`@`localhost` PROCEDURE `InsertarCompra` (IN `p_id_compra` VARCHAR(20), IN `p_doc_proveedor` BIGINT, IN `p_id_producto` INT, IN `p_precio_total` DOUBLE, IN `p_fecha_compra` DATE, IN `p_cantidad` INT)   BEGIN
-    DECLARE v_existe_proveedor INT;
-    DECLARE v_existe_producto INT;
-
-    -- Verificar si el proveedor existe
-    SELECT COUNT(*) INTO v_existe_proveedor
-    FROM proveedor
-    WHERE documento_NIT = p_doc_proveedor;
-
-    IF v_existe_proveedor = 0 THEN
-        -- Aquí puedes manejar la situación si el proveedor no existe
-        SIGNAL SQLSTATE '45000'
-        SET MESSAGE_TEXT = 'El proveedor con documento NIT especificado no existe';
-    END IF;
-
-    -- Verificar si el producto existe
-    SELECT COUNT(*) INTO v_existe_producto
-    FROM producto
-    WHERE id_producto = p_id_producto;
-
-    IF v_existe_producto = 0 THEN
-        -- Aquí puedes manejar la situación si el producto no existe
-        SIGNAL SQLSTATE '45000'
-        SET MESSAGE_TEXT = 'El producto con ID especificado no existe';
-    END IF;
-
-    -- Insertar la compra si los datos son válidos
-    INSERT INTO compra (id_compra, doc_proveedor, id_producto, precio_total, fecha_compra, cantidad) 
-    VALUES (p_id_compra, p_doc_proveedor, p_id_producto, p_precio_total, p_fecha_compra, p_cantidad);
-
-END$$
-
-CREATE DEFINER=`root`@`localhost` PROCEDURE `InsertarEmpleado` (IN `p_documento_emp` INT, IN `p_nom_empleado` VARCHAR(50), IN `p_ape_empleado` VARCHAR(50), IN `p_direccion` VARCHAR(50), IN `p_telefono` BIGINT, IN `p_id_cargos` INT)   BEGIN
-    INSERT INTO empleado (documento_emp, nom_empleado, ape_empleado, direccion, telefono, id_cargos) 
-    VALUES (p_documento_emp, p_nom_empleado, p_ape_empleado, p_direccion, p_telefono, p_id_cargos);
-END$$
-
-CREATE DEFINER=`root`@`localhost` PROCEDURE `InsertarFactura` (IN `p_id_servicio` INT, IN `p_id_producto` INT, IN `p_precio_total` FLOAT, IN `p_descuento` INT, IN `p_fecha_factura` DATE, IN `p_cantidad` INT)   BEGIN
-    INSERT INTO factura (id_servicio, id_producto, precio_total, descuento, fecha_factura, cantidad)
-    VALUES (p_id_servicio, p_id_producto, p_precio_total, p_descuento, p_fecha_factura, p_cantidad);
-END$$
-
-CREATE DEFINER=`root`@`localhost` PROCEDURE `InsertarProducto` (IN `p_nom_producto` VARCHAR(45), IN `p_codigo` VARCHAR(45), IN `p_descripcion` VARCHAR(45), IN `p_precio_unitario` INT, IN `p_existencias` INT)   BEGIN
-    INSERT INTO producto (nom_producto, codigo, descripcion, precio_unitario, existencias) 
-    VALUES (p_nom_producto, p_codigo, p_descripcion, p_precio_unitario, p_existencias);
-END$$
-
-CREATE DEFINER=`root`@`localhost` PROCEDURE `InsertarProveedor` (IN `p_documento_NIT` BIGINT(16), IN `p_nom_proveedor` VARCHAR(70), IN `p_apellido_sociedad` VARCHAR(70), IN `p_direccion` VARCHAR(100), IN `p_telefono` BIGINT(15), IN `p_id_tipo_proveedor` INT(10))   BEGIN
-    INSERT INTO proveedor (documento_NIT, nom_proveedor, apellido_sociedad, direccion, telefono, id_tipo_proveedor) 
-    VALUES (p_documento_NIT, p_nom_proveedor, p_apellido_sociedad, p_direccion, p_telefono, p_id_tipo_proveedor);
-END$$
-
-CREATE DEFINER=`root`@`localhost` PROCEDURE `InsertarServicio` (IN `p_observaciones` VARCHAR(200), IN `p_mantenimiento` VARCHAR(70), IN `p_cedula_cliente` INT, IN `p_documento_emp` INT)   BEGIN
-    INSERT INTO servicios (observaciones, mantenimiento, cedula_cliente, documento_emp) 
-    VALUES (p_observaciones, p_mantenimiento, p_cedula_cliente, p_documento_emp);
-END$$
-
-CREATE DEFINER=`root`@`localhost` PROCEDURE `InsertarTipoProveedor` (IN `p_tipo_proveedor` VARCHAR(50), IN `p_descripcion` VARCHAR(70))   BEGIN
-    INSERT INTO tipo_proveedor (tipo_proveedor, descripcion) VALUES (p_tipo_proveedor, p_descripcion);
-END$$
-
-CREATE DEFINER=`root`@`localhost` PROCEDURE `Insertar_Cargo` (IN `In_tipo_cargo` VARCHAR(50))   BEGIN
-    INSERT INTO cargos (tipo_cargo) VALUES (In_tipo_cargo);
-END$$
-
-DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -272,7 +41,8 @@ INSERT INTO `cargos` (`id_cargos`, `tipo_cargo`) VALUES
 (2, 'Contador'),
 (3, 'Auxiliar II'),
 (4, 'Mecanico'),
-(5, 'Ayudante II');
+(5, 'Ayudante II'),
+(6, 'Revisor Fiscal');
 
 -- --------------------------------------------------------
 
@@ -311,6 +81,7 @@ INSERT INTO `cliente` (`cedula`, `nom_cliente`, `ape_cliente`, `telefono`, `dire
 --
 
 CREATE TABLE `compra` (
+  `id_compra_cons` int(5) NOT NULL,
   `id_compra` varchar(20) NOT NULL,
   `doc_proveedor` bigint(15) NOT NULL,
   `id_producto` int(10) NOT NULL,
@@ -323,12 +94,12 @@ CREATE TABLE `compra` (
 -- Volcado de datos para la tabla `compra`
 --
 
-INSERT INTO `compra` (`id_compra`, `doc_proveedor`, `id_producto`, `precio_total`, `fecha_compra`, `cantidad`) VALUES
-('', 666514, 1, 26600, '2023-07-18', 6),
-('534556', 666514, 2, 5345.54, '2023-06-29', 1),
-('F206', 666514, 1, 2000, '2023-06-15', 2),
-('LLA005', 666514, 1, 700000, '2024-06-14', 5),
-('MD', 56453542, 3, 700000, '2024-06-14', 6);
+INSERT INTO `compra` (`id_compra_cons`, `id_compra`, `doc_proveedor`, `id_producto`, `precio_total`, `fecha_compra`, `cantidad`) VALUES
+(1, 'FAC-857145', 666514, 1, 26600, '2023-07-18', 6),
+(2, 'FAC-857145', 666514, 2, 5345.54, '2023-06-29', 1),
+(3, 'FAC-857143', 666514, 1, 2000, '2023-06-15', 2),
+(4, 'FAC-857147', 666514, 1, 700000, '2024-06-14', 5),
+(5, 'MD-7517', 56453542, 3, 700000, '2024-06-14', 6);
 
 -- --------------------------------------------------------
 
@@ -364,6 +135,7 @@ INSERT INTO `empleado` (`documento_emp`, `nom_empleado`, `ape_empleado`, `direcc
 
 CREATE TABLE `factura` (
   `id_factura` int(10) NOT NULL,
+  `num_fac_serv` varchar(20) NOT NULL,
   `id_servicio` int(10) NOT NULL,
   `id_producto` int(10) NOT NULL,
   `precio_total` float NOT NULL,
@@ -376,16 +148,16 @@ CREATE TABLE `factura` (
 -- Volcado de datos para la tabla `factura`
 --
 
-INSERT INTO `factura` (`id_factura`, `id_servicio`, `id_producto`, `precio_total`, `descuento`, `fecha_factura`, `cantidad`) VALUES
-(1, 2, 2, 60000, 0, '2023-06-15 05:00:00', 2),
-(2, 3, 1, 200000, 0, '2023-07-11 05:00:00', 2),
-(3, 4, 2, 600000, 0, '2023-07-14 05:00:00', 2),
-(4, 1, 1, 700000, 0, '2023-07-12 05:00:00', 2),
-(6, 6, 1, 100000, 0, '2023-07-13 05:00:00', 1),
-(7, 7, 2, 2000000, 0, '2023-07-07 05:00:00', 1),
-(8, 8, 3, 70000, 0, '2023-07-06 05:00:00', 1),
-(9, 2, 3, 200000, 0, '2023-07-18 05:00:00', 2),
-(10, 7, 1, 700000, 0, '2024-06-13 05:00:00', 2);
+INSERT INTO `factura` (`id_factura`, `num_fac_serv`, `id_servicio`, `id_producto`, `precio_total`, `descuento`, `fecha_factura`, `cantidad`) VALUES
+(1, 'PTB-006', 2, 2, 60000, 0, '2023-06-15 05:00:00', 2),
+(2, 'PTB-005', 3, 1, 200000, 0, '2023-07-11 05:00:00', 2),
+(3, 'PTB-004', 4, 2, 600000, 0, '2023-07-14 05:00:00', 2),
+(4, 'PTB-007', 1, 1, 700000, 0, '2023-07-12 05:00:00', 2),
+(6, 'PTB-003', 6, 1, 100000, 0, '2023-07-13 05:00:00', 1),
+(7, 'PTB-002', 7, 2, 2000000, 0, '2023-07-07 05:00:00', 1),
+(8, 'PTB-001', 8, 3, 70000, 0, '2023-07-06 05:00:00', 1),
+(9, 'PTB-006', 2, 3, 200000, 0, '2023-07-18 05:00:00', 2),
+(10, 'PTB-002', 7, 1, 700000, 0, '2024-06-13 05:00:00', 2);
 
 --
 -- Disparadores `factura`
@@ -477,7 +249,7 @@ CREATE TABLE `servicios` (
 --
 
 INSERT INTO `servicios` (`id_servicios`, `observaciones`, `mantenimiento`, `cedula_cliente`, `documento_emp`) VALUES
-(1, 'se realiza cambio de aciete', 'bien', 1000120253, 1000120253),
+(1, 'se realiza cambio de aciete', '', 1000120253, 1000120253),
 (2, 'se ajusta la moto', 'bien todo ok', 33435229, 966544),
 (3, 'Ser verifica la cantidad de acite', 'ok', 3356241, 1000120253),
 (4, 'verificacion del motor', 'ok', 1000120253, 1000120253),
@@ -570,7 +342,7 @@ ALTER TABLE `cliente`
 -- Indices de la tabla `compra`
 --
 ALTER TABLE `compra`
-  ADD PRIMARY KEY (`id_compra`),
+  ADD PRIMARY KEY (`id_compra_cons`),
   ADD KEY `doc_proveerdor` (`doc_proveedor`),
   ADD KEY `id_producto` (`id_producto`);
 
@@ -630,7 +402,13 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `cargos`
 --
 ALTER TABLE `cargos`
-  MODIFY `id_cargos` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_cargos` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de la tabla `compra`
+--
+ALTER TABLE `compra`
+  MODIFY `id_compra_cons` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `factura`
